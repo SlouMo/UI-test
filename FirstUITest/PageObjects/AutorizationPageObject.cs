6c88859c6812d70b29d62efbac14a0fcf0019bde
Vladimir Kuznetsov <vl.kuznetsov@bars-open.ru>
2021-10-05 10:28:43 +0300

    Add PageObject for page Autorization

---
+++ FirstUITest/PageObjects/AutorizationPageObject.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstUITest.PageObjects
+{
+    class AutorizationPageObject
+    {
+        private IWebDriver _webDriver;
+        private WebDriverWait _wait;
+
+        private readonly By _lodinField = By.XPath("//*[@id='login']");
+        private readonly By _passwordField = By.XPath("//*[@id='password']");
+        private readonly By _autorizationButton = By.XPath("//*[@type='submit']");
+
+        public AutorizationPageObject(IWebDriver webDriver)
+        {
+            _webDriver = webDriver;
+            _wait = new WebDriverWait(_webDriver, TimeSpan.FromSeconds(600));
+        }
+
+        public void Autoruzation(string login, string password)
+        {
+            _wait.Until(ExpectedConditions.ElementIsVisible(_autorizationButton));
+
+            _webDriver.FindElement(_lodinField).SendKeys(login);
+            _webDriver.FindElement(_passwordField).SendKeys(password);
+            _webDriver.FindElement(_autorizationButton).Click();
+        }
+    }
+}
--- FirstUITest/Tests/UnitTest1.cs
+++ FirstUITest/Tests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using FirstUITest.PageObjects;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
@@ -12,13 +13,10 @@ namespace FirstUITest
         private WebDriverWait _wait;
 
         private const string _URL = "https://obp.bars.group/bp_RC/login";
+        private const string _expectedFIO = "Кузнецов Владимир Алексеевич";
         private const string _password = "123";
         private const string _login = "bars_vl.kuznetsov";
-        private const string _expectedFIO = "Кузнецов Владимир Алексеевич";
 
-        private readonly By _lodinField = By.XPath("//*[@id='login']");
-        private readonly By _passwordField = By.XPath("//*[@id='password']");
-        private readonly By _autorizationButton = By.XPath("//*[@type='submit']");
         private readonly By _FIOview = By.XPath("//*[@class='profile']/child::p");
 
 
@@ -39,12 +37,10 @@ namespace FirstUITest
         [Test]
         public void Test()
         {
-            //Thread.Sleep(1000);
-            _wait.Until(ExpectedConditions.ElementIsVisible(_autorizationButton));
 
-            _driver.FindElement(_lodinField).SendKeys(_login);
-            _driver.FindElement(_passwordField).SendKeys(_password);
-            _driver.FindElement(_autorizationButton).Click();
+            var autorization = new AutorizationPageObject(_driver);
+            autorization.Autoruzation(_login, _password);
+
             _wait.Until(ExpectedConditions.ElementIsVisible(_FIOview));
 
             var actualFIO = _driver.FindElement(_FIOview).Text;
